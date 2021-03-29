05d2a2c0da7d69539903efadd744864b965e5c7e
kirill <kirill.prokofiev@intel.com>
2021-03-29 23:15:42 +0300

    model wraper works

--- torchdet3d/builders/model_builder.py
+++ torchdet3d/builders/model_builder.py
@@ -8,15 +8,19 @@ from efficientnet_lite_pytorch import EfficientNet
 from efficientnet_lite_pytorch.utils import get_model_params
 from efficientnet_lite0_pytorch_model import EfficientnetLite0ModelFile
 
-from torchdet3d.models import mobilenetv3_large, MobileNetV3
+from torchdet3d.models import mobilenetv3_large, MobileNetV3, init_pretrained_weights
 from  torchdet3d.utils import load_pretrained_weights
 
 def build_model(cfg):
     if cfg.model.name.startswith('efficientnet'):
         weights_path = EfficientnetLite0ModelFile.get_model_file_path()
         blocks_args, global_params = get_model_params(cfg.model.name, override_params=None)
-        kwargs = dict(blocks_args=blocks_args, global_params=global_params)
-        model = model_wraper(model_class=EfficientNet, **kwargs)
+        kwargs = dict()
+        model = model_wraper(model_class=EfficientNet,
+                             num_classes=cfg.model.num_classes,
+                             blocks_args=blocks_args,
+                             global_params=global_params)
+
         if cfg.model.pretrained:
             load_pretrained_weights(model, weights_path)
 
@@ -39,16 +43,16 @@ def build_model(cfg):
         [5,   6, 160, 1, 1, 1],
         [5,   6, 160, 1, 1, 1]
     ], mode='large')
-            model = model_wraper(model_class=MobileNetV3, **kwargs)
+            model = model_wraper(model_class=MobileNetV3, num_classes=cfg.model.num_classes, **kwargs)
             if cfg.model.pretrained:
-                model.init_pretrained_weights()
-            model = mobilenetv3_large(pretrained=cfg.model.pretrained, num_classes=cfg.model.num_classes, resume=cfg.model.load_weights)
+                init_pretrained_weights(model, key='mobilenetv3_large')
+            # model = mobilenetv3_large(pretrained=cfg.model.pretrained, num_classes=cfg.model.num_classes, resume=cfg.model.load_weights)
 
     return model
 
 def model_wraper(model_class, num_points=18, num_classes=1, pooling_mode='avg', **kwargs):
     class ModelWrapper(model_class):
-        def __init__(self, **kwargs):
+        def __init__(self, output_channel=1280,**kwargs):
             # if block_args and global_params:
             #     super().__init__(**kwargs)
             # else:
@@ -56,25 +60,17 @@ def model_wraper(model_class, num_points=18, num_classes=1, pooling_mode='avg', 
 
             self.regressors = nn.ModuleList()
             for trg_id, trg_num_classes in enumerate(range(num_classes)):
-                self.regressors.append(self._init_regressors(exp_size, output_channel))
-
+                self.regressors.append(self._init_regressors(output_channel))
             self.classifier = nn.Sequential(
-                nn.Linear(exp_size, output_channel),
-                h_swish(),
                 nn.Dropout(0.5),
-                nn.Linear(output_channel, self.num_classes),
+                nn.Linear(output_channel, num_classes),
             )
 
             self.sigmoid = nn.Sigmoid()
 
         @staticmethod
-        def _init_regressors(exp_size, output_channel):
-            return nn.Sequential(
-                nn.Linear(exp_size, output_channel),
-                nn.LeakyReLU(),
-                nn.BatchNorm1d(output_channel),
-                nn.Linear(output_channel, num_points),
-            )
+        def _init_regressors(output_channel):
+            return nn.Linear(output_channel, num_points)
 
         @staticmethod
         def _glob_feature_vector(x, mode, reduce_dims=True):
@@ -95,10 +91,11 @@ def model_wraper(model_class, num_points=18, num_classes=1, pooling_mode='avg', 
 
         def forward(self, x, cats):
             features = self.extract_features(x)
-            pooled_features = _glob_feature_vector(features, mode=pooling_mode)
+            pooled_features = self._glob_feature_vector(features, mode=pooling_mode)
+            ic(pooled_features.shape, cats.shape)
             kp = torch.cat([self.regressors[id](sample) for id, sample in zip(cats, pooled_features)], 0)
             kp = self.sigmoid(kp)
-            targets = self.classifier(x) if num_classes > 1 else torch.zeros(kp.size(0), num_classes).to(x.device)
+            targets = self.classifier(pooled_features) if num_classes > 1 else torch.zeros(kp.size(0), num_classes).to(x.device)
             return kp.view(x.size(0), num_points // 2, 2), targets
 
     model = ModelWrapper(**kwargs)
@@ -107,7 +104,9 @@ def model_wraper(model_class, num_points=18, num_classes=1, pooling_mode='avg', 
 def test(cfg):
     model = build_model(cfg)
     img = torch.rand(128,3,224,224)
-    ic(model(img).shape)
+    cats = torch.randint(0,5,(128,))
+    out = model(img, cats)
+    ic(out[0].shape, out[1].shape)
 
 if __name__ == "__main__":
     from torchdet3d.utils import read_py_config
