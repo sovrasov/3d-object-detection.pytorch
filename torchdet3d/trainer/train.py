3cd1f9e24e6d40505ad4c316fcc1b25bfe841558
kirill <kirill.prokofiev@intel.com>
2021-04-15 14:09:59 +0300

    added eta to train

--- torchdet3d/trainer/train.py
+++ torchdet3d/trainer/train.py
@@ -1,3 +1,6 @@
+import time
+import datetime
+
 from tqdm import tqdm
 from dataclasses import dataclass
 import torch
@@ -29,13 +32,20 @@ class Trainer:
         ADD_meter = AverageMeter()
         SADD_meter = AverageMeter()
         ACC_meter = AverageMeter()
+        batch_time = AverageMeter()
 
         # switch to train mode and train one epoch
         self.model.train()
         self.num_iters = len(self.train_loader)
-
+        end = time.time()
         loop = tqdm(enumerate(self.train_loader), total=self.num_iters, leave=False)
         for it, (imgs, gt_kp, gt_cats) in loop:
+            # compute eta
+            batch_time.update(time.time() - end)
+            nb_this_epoch = self.num_iters - (it + 1)
+            nb_future_epochs = (self.max_epoch - (epoch + 1)) * self.num_iters
+            eta_seconds = batch_time.avg * (nb_this_epoch+nb_future_epochs)
+            eta_str = str(datetime.timedelta(seconds=int(eta_seconds)))
             # put image and keypoints on the appropriate device
             imgs, gt_kp, gt_cats = self.put_on_device([imgs, gt_kp, gt_cats], self.device)
             # compute output and loss
@@ -73,6 +83,8 @@ class Trainer:
             if ((it % self.print_freq == 0) or (it == self.num_iters-1)):
                 print(
                         'epoch: [{0}/{1}][{2}/{3}]\t'
+                        'time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                        'eta {eta}\t'
                         'cls acc {accuracy.val:.3f} ({accuracy.avg:.3f})\t'
                         'ADD {ADD.val:.4f} ({ADD.avg:.4f})\t'
                         'SADD {SADD.val:.4f} ({SADD.avg:.4f})\t'
@@ -82,17 +94,21 @@ class Trainer:
                             self.max_epoch,
                             it,
                             self.num_iters,
+                            batch_time=batch_time,
+                            eta=eta_str,
                             accuracy=ACC_meter,
                             ADD=ADD_meter,
                             SADD=SADD_meter,
                             losses=losses,
                             lr=self.optimizer.param_groups[0]['lr'])
                         )
+
+            end = time.time()
             if (self.debug and it == self.debug_steps):
                 break
 
         # save every 10 epoch
-        if self.save_chkpt and self.save_freq % 10 == 0 and not self.debug:
+        if self.save_chkpt and self.epoch % self.save_freq == 0 and not self.debug:
             save_snap(self.model, self.optimizer, epoch, self.log_path)
 
     @staticmethod
